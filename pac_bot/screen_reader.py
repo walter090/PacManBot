e7a062470ec3df22819b1c6fe7a1408e69cdfd44
walter_wu090 <walter.wu090@gmail.com>
2017-09-16 16:03:59 -0400

    Simplify screen reader

--- pac_bot/screen_reader.py
+++ pac_bot/screen_reader.py
@@ -5,37 +5,28 @@ import numpy as np
 import pyscreenshot as ImageGrab
 
 
-class ScreenReader(object):
-    def __init__(self, origin, terminus):
-        """Constructor for the screen reader class, this constructor takes two
-        arguments, origin and terminus each of which is a coordinate on the screen.
+def capture(origin, terminus, quit_on='q', verbose=False):
+    """Start capturing the designated part of the screen.
 
-        Args:
-            origin: A tuple or list that contains two elements; this is the coordinate for
-                the starting point on the region to be read. This should be the top left
-                corner of the box.
-            terminus: A tuple or list that contains two elements; this is the coordinate for
-                the ending point on the region to be read. This should be the bottom right
-                corner of the box.
-        """
-        self.origin = origin
-        self.terminus = terminus
+    Args:
+        origin: A tuple or list that contains two elements; this is the coordinate for
+            the starting point on the region to be read. This should be the top left
+            corner of the box.
+        terminus: A tuple or list that contains two elements; this is the coordinate for
+            the ending point on the region to be read. This should be the bottom right
+            corner of the box.
+        quit_on: Quit capturing on this keystroke, defaults q.
+        verbose: Set to True to print time each frame.
 
-    def capture(self, quit_on='q', verbose=False):
-        """Start capturing the designated part of the screen.
-
-        Args:
-            quit_on: Quit capturing on this keystroke, defaults q.
-
-        Returns:
-            None
-        """
-        while True:
-            frame_start_time = time.time()
-            screen = np.array(ImageGrab.grab(bbox=(*self.origin, *self.terminus)))
-            cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-            if verbose:
-                print('Time since last frame: {}'.format(time.time() - frame_start_time))
-            if cv2.waitKey(30) & 0xFF == ord(quit_on):
-                cv2.destroyAllWindows()
-                break
+    Returns:
+        None
+    """
+    while True:
+        frame_start_time = time.time()
+        screen = np.array(ImageGrab.grab(bbox=(origin, terminus)))
+        cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if verbose:
+            print('Time since last frame: {}'.format(time.time() - frame_start_time))
+        if cv2.waitKey(30) & 0xFF == ord(quit_on):
+            cv2.destroyAllWindows()
+            break
