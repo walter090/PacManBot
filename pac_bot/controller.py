012536e5596ebdbe8c70983b77f5cd4cf5d7929f
walter_wu090 <walter.wu090@gmail.com>
2017-09-16 16:05:43 -0400

    Add controller

---
+++ pac_bot/controller.py
@@ -0,0 +1,36 @@
+import pyautogui as pag
+import time
+
+
+def commence(countdown=5):
+    """Call this method each time before a game to start a countdown
+
+    Args:
+        countdown: Time in seconds before any operations. Defaults 5 sec.
+
+    Returns:
+        None
+    """
+    for i in range(countdown):
+        print(5 - i, end=' ')
+        time.sleep(0.25)
+        print('.', end=' ')
+        time.sleep(0.25)
+        print('.', end=' ')
+        time.sleep(0.5)
+
+
+def left():
+    pag.press('left')
+
+
+def right():
+    pag.press('right')
+
+
+def up():
+    pag.press('up')
+
+
+def down():
+    pag.press('down')
