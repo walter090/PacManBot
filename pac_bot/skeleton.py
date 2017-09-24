fd23bb818f323d2179d16ee6590d3c5639cc2109
walter_wu090 <walter.wu090@gmail.com>
2017-09-24 15:20:22 -0400

    Add functions for flatten matrices and fully connected layer

--- pac_bot/skeleton.py
+++ pac_bot/skeleton.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -118,3 +119,58 @@ def batch_normalize(x, epsilon=1e-5):
                                                scale=scale,
                                                variance_epsilon=epsilon)
         return normalized
+
+
+def flatten(x):
+    """Flatten a tensor for the fully connected layer.
+    Each image in a batch is flattened.
+
+    Args:
+        x(Tensor): 4-D tensor of shape [batch, height, width, channels] to be flattened
+            to the shape of [batch, height * width * channels]
+
+    Returns:
+        Flattened tensor.
+    """
+    return tf.reshape(x, shape=[-1, np.prod(x.get_shape().as_list()[1:])])
+
+
+def fully_conn(x,
+               num_output,
+               name='fc',
+               activation='lrelu',
+               keep_prob=1.):
+    """Fully connected layer, this is is last parts of convnet.
+    Fully connect layer requires each image in the batch be flattened.
+
+    Args:
+        x: Input from the previous layer.
+        num_output: Output size of the fully connected layer.
+        name: Name for the fully connected layer variable scope.
+        activation: Set to True to add a leaky relu after fully connected
+            layer. Set this argument to False if this is the final layer.
+        keep_prob: Keep probability for dropout layers, if keep probability is 1
+            there is no dropout. Defaults 1.
+
+    Returns:
+        Output tensor.
+    """
+    with tf.variable_scope(name):
+        weights = tf.get_variable(name='fc_w',
+                                  shape=[x.get_shape().as_list()[-1], num_output],
+                                  initializer=tf.random_normal_initializer(stddev=0.02))
+        biases = tf.get_variable(name='fc_b',
+                                 shape=[num_output],
+                                 initializer=tf.zeros_initializer())
+
+        output = tf.nn.bias_add(tf.matmul(x, weights), biases)
+        output = tf.nn.dropout(output, keep_prob=keep_prob)
+
+        if activation == 'sigmoid':
+            output = tf.sigmoid(output)
+        elif activation == 'lrelu':
+            output = lrelu(output)
+        else:
+            pass
+
+        return output
