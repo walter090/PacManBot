1d3c1571a1812d440893b3e23dbb51dbca7b2930
walter_wu090 <walter.wu090@gmail.com>
2017-10-03 00:56:08 -0400

    Implement encoding conv net

---
+++ pac_bot/network.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from . import layers
+
+
+class Network(object):
+    def __init__(self, action_size, name='network'):
+        with tf.variable_scope(name):
+            self.action_size = action_size
+
+    @staticmethod
+    def encoding_network(x, layers_config=None, activation='lrelu', name='encoding'):
+        """Build the encoding network.
+
+        Args:
+            x: Input tensor.
+            layers_config: Configuration for each convolution layer;
+                each layer is a list with three elements: filter size, stride, and
+                number of output channels.
+            activation: Choose activation function, between 'lrelu' and 'elu.'
+            name: Name of variable scope.
+
+        Returns:
+            fc: Output from the conv net.
+        """
+        with tf.variable_scope(name):
+            if layers_config is None:
+                layers_config = [
+                    # Filter size, stride, num output channels
+                    [(8, 8), (2, 2), 32],
+                    [(8, 8), (4, 4), 64],
+                    [(4, 4), (2, 2), 128],
+                    [(4, 4), (2, 2), 256],
+                ]
+
+            conv_output = x
+            for layer in layers_config:
+                conv_output = layers.conv_layer(x=conv_output,
+                                                conv_ksize=layer[0],
+                                                conv_stride=layer[1],
+                                                out_channels=layer[2],
+                                                activation=activation)
+            flattened = layers.flatten(conv_output)
+            fc = layers.fully_conn(x=flattened, num_output=516)
+            fc = layers.fully_conn(x=fc, num_output=256)
+
+            return fc
+
+    def lstm_network(self):
+        raise NotImplementedError
